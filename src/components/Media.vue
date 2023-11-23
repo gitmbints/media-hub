46b0419b6bb904ed16734585c8b438e668f40e8d
Ambinintsoa <rahmbints@gmail.com>
2023-11-23 17:23:15 +0300

    Make the media component on separate file

--- src/components/Media.vue
+++ src/components/Media.vue
@@ -1,17 +1,16 @@
 <script setup>
-import useAuthUser from "../composable/UseAuthUser";
-
 defineProps({
+	userId: String,
 	name: String,
 });
 
-console.log(name);
-
-const { user } = useAuthUser();
-const MEDIA_URL = `https://lybippqhemldffwoqxyd.supabase.co/storage/v1/object/public/media/${user.value.id}/${name}`;
+const MEDIA_URL =
+	"https://lybippqhemldffwoqxyd.supabase.co/storage/v1/object/public/media/";
 </script>
 <template>
-	<div class="w-full h-full">
-		<img :src="MEDIA_URL" class="w-full h-full object-cover" />
+	<div class="w-full h-full rounded overflow-hidden">
+		<img
+			:src="MEDIA_URL + '/' + userId + '/' + name"
+			class="w-full h-full object-cover" />
 	</div>
 </template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,17 @@
 <script setup>
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import useAuthUser from "../composable/UseAuthUser";
 import BaseLayout from "./BaseLayout.vue";
 import { Badge } from "@/components/ui/badge";
 import UploadButton from "@/components/UploadButton.vue";
 import useSupabase from "@/composable/UseSupabase";
+import Media from "@/components/Media.vue";
 
 const { user } = useAuthUser();
 const { supabase } = useSupabase();
 
 const file = ref(null);
 const medias = ref([]);
-const MEDIA_URL =
-	"https://lybippqhemldffwoqxyd.supabase.co/storage/v1/object/public/media/";
 
 const uploadFile = async (event) => {
 	file.value = event.target.files[0];
@@ -56,6 +55,17 @@ const getFile = async () => {
 onMounted(async () => {
 	await getFile();
 });
+
+const imagesCount = computed(() => {
+	const images = medias.value.filter((media) =>
+		media.metadata.mimetype.includes("image/")
+	);
+	return images.length;
+});
+
+const mediasCount = computed(() => {
+	return medias.value.length;
+});
 </script>
 
 <template>
@@ -67,11 +77,11 @@ onMounted(async () => {
 					<ul class="flex flex-col space-y-2">
 						<li class="flex items-center justify-between p-1">
 							<span>Tous</span>
-							<Badge class="bg-emerald-400">16</Badge>
+							<Badge class="bg-emerald-400">{{ mediasCount }}</Badge>
 						</li>
 						<li class="flex items-center justify-between p-1">
 							<span>Images</span>
-							<Badge class="bg-emerald-400">10</Badge>
+							<Badge class="bg-emerald-400">{{ imagesCount }}</Badge>
 						</li>
 						<li class="flex items-center justify-between p-1">
 							<span>Vidéos</span>
@@ -79,12 +89,14 @@ onMounted(async () => {
 						</li>
 					</ul>
 				</aside>
-				<div class="basis-10/12 p-4 pl-8 grid grid-cols-6 gap-x-6 gap-y-4">
+				<div
+					class="basis-10/12 p-4 pl-8 grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-6 gap-y-4">
 					<UploadButton @upload-file="uploadFile" />
-					<img
+					<Media
 						v-for="media in medias"
-						:src="MEDIA_URL + '/' + user.id + '/' + media.name"
-						class="w-full h-full object-cover" />
+						:key="media.name"
+						:name="media.name"
+						:user-id="user.id" />
 				</div>
 			</div>
 		</BaseLayout>
