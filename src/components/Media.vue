a81d46f94c7dafc14f08cb6354a3900cfc5ab997
Ambinintsoa <rahmbints@gmail.com>
2023-11-24 17:42:46 +0300

    Should implement delete features

--- src/components/Media.vue
+++ src/components/Media.vue
@@ -1,16 +1,29 @@
 <script setup>
 defineProps({
 	userId: String,
-	name: String,
+	fileName: String,
 });
+
+defineEmits(["deleteMedia"]);
 
 const MEDIA_URL =
 	"https://lybippqhemldffwoqxyd.supabase.co/storage/v1/object/public/media/";
 </script>
 <template>
-	<div class="w-full h-full rounded overflow-hidden">
+	<div class="relative group w-full h-full rounded overflow-hidden">
 		<img
-			:src="MEDIA_URL + '/' + userId + '/' + name"
-			class="w-full h-full object-cover" />
+			:src="MEDIA_URL + '/' + userId + '/' + fileName"
+			class="w-full h-full aspect-auto"
+		/>
+		<span
+			class="absolute bg-black/20 inset-0 hidden group-hover:grid place-items-center"
+		>
+			<button
+				@click="$emit('deleteMedia', fileName)"
+				class="border border-solid border-white p-1 rounded bg-transparent font-semibold text-white text-xs text-center"
+			>
+				Effacer
+			</button>
+		</span>
 	</div>
 </template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -46,6 +46,7 @@ const getFile = async () => {
 		if (error) {
 			console.error(error);
 		}
+
 		medias.value = data;
 	} catch (error) {
 		console.error(error.message);
@@ -66,6 +67,26 @@ const imagesCount = computed(() => {
 const mediasCount = computed(() => {
 	return medias.value.length;
 });
+
+const isLoading = computed(() => {
+	return mediasCount !== 0 ? false : true;
+});
+
+const deleteMedia = async (fileName) => {
+	try {
+		const { data, error } = await supabase.storage
+			.from("media")
+			.remove([`${user.value.id}/${fileName}`]);
+
+		if (error) {
+			console.error(error);
+		}
+
+		//const dataFileName = data[0].name.split("/")[1];
+	} catch (error) {
+		console.error(error.message);
+	}
+};
 </script>
 
 <template>
@@ -73,7 +94,8 @@ const mediasCount = computed(() => {
 		<BaseLayout>
 			<div class="flex flex-row">
 				<aside
-					class="basis-2/12 pr-4 py-4 border border-solid border-t-0 border-l-0 border-b-0 border-slate-400">
+					class="basis-2/12 pr-4 py-4 border border-solid border-t-0 border-l-0 border-b-0 border-slate-400"
+				>
 					<ul class="flex flex-col space-y-2">
 						<li class="flex items-center justify-between p-1">
 							<span>Tous</span>
@@ -90,13 +112,18 @@ const mediasCount = computed(() => {
 					</ul>
 				</aside>
 				<div
-					class="basis-10/12 p-4 pl-8 grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-6 gap-y-4">
+					class="basis-10/12 p-4 pl-8 grid grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-4"
+				>
 					<UploadButton @upload-file="uploadFile" />
+					<p v-if="isLoading" class="text-center">Loading...</p>
 					<Media
+						v-else
 						v-for="media in medias"
 						:key="media.name"
-						:name="media.name"
-						:user-id="user.id" />
+						:fileName="media.name"
+						:user-id="user.id"
+						@delete-media="deleteMedia"
+					/>
 				</div>
 			</div>
 		</BaseLayout>
