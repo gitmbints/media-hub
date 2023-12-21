017d8022323ecbc2e6901d513b4a57453052b629
Ambinintsoa <rahmbints@gmail.com>
2023-12-21 17:07:51 +0300

    feat: refactor Media component

--- src/components/Media.vue
+++ src/components/Media.vue
@@ -1,10 +1,14 @@
 <script setup>
-defineProps({
+const props = defineProps({
 	userId: String,
 	fileName: String,
 });
 
-defineEmits(["deleteMedia"]);
+const emit = defineEmits(["deleteMedia"]);
+
+const handleDelete = () => {
+	emit("deleteMedia", props.fileName);
+};
 
 const MEDIA_URL =
 	"https://lybippqhemldffwoqxyd.supabase.co/storage/v1/object/public/media";
@@ -12,14 +16,14 @@ const MEDIA_URL =
 <template>
 	<div class="relative group w-full h-40 rounded overflow-hidden">
 		<img
-			:src="MEDIA_URL + '/' + userId + '/store/' + fileName"
+			:src="MEDIA_URL + '/' + props.userId + '/store/' + props.fileName"
 			class="w-full h-full object-cover"
 		/>
 		<span
 			class="absolute bg-black/20 inset-0 hidden group-hover:grid place-items-center"
 		>
 			<button
-				@click="$emit('deleteMedia', fileName)"
+				@click="handleDelete"
 				class="border border-solid border-white py-1 px-2 rounded bg-transparent font-semibold text-white text-xs text-center hover:bg-white/20 transition"
 			>
 				Effacer
